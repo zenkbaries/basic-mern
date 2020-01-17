ecd335c95ca38db3e50cadd9429ccbcbb632f908
John Towery <kbaries@gmail.com>
2020-01-17 12:02:53 -0700

    clean up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 <div className="col-xs-12 col-sm-6">
                     {/* <%= link_to "This isn't enough, I need more", root_path , :class => 'next-quote' %> */}
                 </div>
-                <br /> <br />
+                {/* <br /> <br /> */}
                 <div className="col-xs-12 footer-blurb">
                     Proudly hacked in Denver, CO by <a href="https://github.com/zenkbaries">John Towery</a><br />
                     {/* Want to build your own Splurty Web App - <a href="http://thefirehoseproject.com">Learn how to code here</a> */}
